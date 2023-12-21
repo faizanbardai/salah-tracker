f157cc2ca55fb498769b806c95f57531c608855d
FayJu <faizan.badruddin.bardai@gmail.com>
2023-12-21 17:58:02 +0100

    testing with getProfile function

--- src/service/auth/getProfile.ts
+++ src/service/auth/getProfile.ts
@@ -1,13 +1,11 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 const getProfile = async () => {
-  const res = await fetch("https://salah-tracker.azurewebsites.net/.auth/me");
-  if (res.status !== 200) {
-    return null;
-  } else {
-    const json = await res.json();
-    console.log(json);
-    redirect("/profile");
+  try {
+    const res = await fetch("https://salah-tracker.azurewebsites.net/.auth/me");
+    return res.json();
+  } catch (error) {
+    return { error };
   }
 };
 
