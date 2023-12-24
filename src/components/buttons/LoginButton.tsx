45460369f05b1efd8031b93f3d39618b53e9f659
FayJu <faizan.badruddin.bardai@gmail.com>
2023-12-24 22:10:57 +0100

    removed getProfile function

--- src/components/buttons/LoginButton.tsx
+++ src/components/buttons/LoginButton.tsx
@@ -1,15 +1,7 @@
-import getProfile from "../../service/auth/getProfile";
-
 export default async function LoginButton() {
-  const profile = await getProfile();
-  console.log(profile);
   return (
     <div>
       <a href="/.auth/login/google">Log in with Google</a>
-      {/* Display profile json as text */}
-      <div>
-        <pre>{JSON.stringify(profile, null, 2)}</pre>
-      </div>
     </div>
   );
 }
