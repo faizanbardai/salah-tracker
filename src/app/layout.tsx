fe6869c68da521cddb20264adad66ee56cbe0c67
FayJu <faizan.badruddin.bardai@gmail.com>
2024-01-02 17:46:45 +0100

    PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,20 @@ import "./globals.css";
 import { Providers } from "../providers/Providers";
 import NavBar from "../components/sections/NavBar";
 import Script from "next/script";
+import type { Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Salah Tracker",
   description: "Track your salah",
+  // https://uxwing.com/secure-date-calendar-icon/
   manifest: "/manifest.json",
-  themeColor: "#000000",
+  icons: "/secure-date-calendar-icon.png",
+};
+
+export const viewport: Viewport = {
+  themeColor: "black",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
