9dcf26294fe19a1d4a44954ae501c1bf2e91027b
FayJu <faizan.badruddin.bardai@gmail.com>
2024-01-02 09:37:30 +0100

    Add manifest and service worker scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,20 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../providers/Providers";
 import NavBar from "../components/sections/NavBar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Salah Tracker",
   description: "Track your salah",
+  manifest: "/manifest.json",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script src="/register-sw.js" />
       <body className={`h-screen ${inter}`}>
         <Providers>
           <NavBar />
