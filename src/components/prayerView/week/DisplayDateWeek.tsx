8a5c46c4b56e8e123d057d412a2199d07e41176a
FayJu <faizan.badruddin.bardai@gmail.com>
2024-01-03 11:31:06 +0100

    Add previous and next week navigation functionality

--- src/components/prayerView/week/DisplayDateWeek.tsx
+++ src/components/prayerView/week/DisplayDateWeek.tsx
@@ -30,10 +30,21 @@ export const DisplayDateWeek = ({ start, end, date, setDate }: DisplayDateWeekPr
     setShowHijriDate(!showHijriDate);
     localStorage.setItem("showHijriDate", String(!showHijriDate));
   }
+  function redirectToLastWeek() {
+    const endOfLastWeek = new Date(start);
+    endOfLastWeek.setDate(endOfLastWeek.getDate() - 1);
+    setDate(endOfLastWeek.toISOString().slice(0, 10));
+  }
+  function redirectToNextWeek() {
+    const startOfNextWeek = new Date(end);
+    startOfNextWeek.setDate(startOfNextWeek.getDate() + 1);
+    setDate(startOfNextWeek.toISOString().slice(0, 10));
+  }
+
   return (
     <div className="flex justify-between pb-3">
       <Button isIconOnly color="success">
-        <MdNavigateBefore size="25" />
+        <MdNavigateBefore size="25" onClick={redirectToLastWeek} />
       </Button>
       <Button
         variant="solid"
@@ -45,7 +56,7 @@ export const DisplayDateWeek = ({ start, end, date, setDate }: DisplayDateWeekPr
         {showHijriDate ? hijriDate : gregorianDate}
       </Button>
       <Button isIconOnly color="success">
-        <MdOutlineNavigateNext size="25" />
+        <MdOutlineNavigateNext size="25" onClick={redirectToNextWeek} />
       </Button>
     </div>
   );
