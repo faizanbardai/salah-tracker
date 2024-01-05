04523a2ae62317d14f1362c125e568c85b3a758b
FayJu <faizan.badruddin.bardai@gmail.com>
2024-01-05 18:05:37 +0100

    Add prayer times API integration and display Fajr and Dhuhr timings

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -3,9 +3,30 @@ import { headers } from "next/headers";
 
 /** @param {NextRequest} req */
 export async function GET(req: NextRequest) {
-  const headersList = headers();
-  const ip_address = headersList.get("x-forwarded-for");
-  const res = await fetch(`http://ip-api.com/json/${ip_address}`);
-  const data = await res.json();
-  return NextResponse.json(data);
+  try {
+    const headersList = headers();
+    const ip_address = headersList.get("x-forwarded-for");
+    let location = await fetch(`http://ip-api.com/json/${ip_address}`);
+    location = await location.json();
+
+    // https://api.aladhan.com/v1/timings/05-01-2024?latitude=48.1663&longitude=11.5683
+    const date = new Date().toLocaleDateString("en-US").split("/").reverse().join("-");
+    const prayerTimesURL = "https://api.aladhan.com/v1/timings/";
+    let prayerTimes = await fetch(
+      `${prayerTimesURL}${date}?latitude=${location.lat}&longitude=${location.lon}`
+    );
+    const prayerTimesData = await (prayerTimes.json() as Promise<PrayerTimesResponse>);
+
+    const timings = prayerTimesData.data.timings;
+
+    return NextResponse.json(timings);
+  } catch (error) {
+    return NextResponse.json({ Fajr: "", Dhuhr: "" });
+  }
+}
+
+interface PrayerTimesResponse {
+  data: {
+    timings: any;
+  };
 }
--- src/components/sections/ClientLocation.tsx
+++ src/components/sections/ClientLocation.tsx
@@ -1,13 +1,7 @@
 import useSWR from "swr";
 
-type ClientLocationProps = {
-  lat: number | null;
-  lon: number | null;
-};
-
 export default function ClientLocation() {
-  const fetcher = (url: string): Promise<ClientLocationProps> =>
-    fetch(url).then((res) => res.json());
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const url = "/api/ip";
   const { data, error, isLoading } = useSWR(url, fetcher, { revalidateOnFocus: false });
   if (error) return <div>Failed to load</div>;
@@ -15,7 +9,7 @@ export default function ClientLocation() {
   if (!data) return <div>Failed to load</div>;
   return (
     <div>
-      lat: {data.lat}, lon: {data.lon}
+      Fajr: {data.Fajr}, Dhuhr: {data.Dhuhr}
     </div>
   );
 }
